3c715e696267fbb7f8c52a95689fbda379523a0e
Weslley <weslleyjonathas88@gmail.com>
2022-06-15 23:10:27 -0300

    fix sql

--- query.sql
+++ query.sql
@@ -1,16 +1,12 @@
-PASSO 1: create database n2-web;
+CREATE DATABASE n2-web;
 
-PASSO 2: 
-create table funcionarios (
-id int primary key not null generated always as identity,
-nome varchar(30) not null,
-sobrenome varchar(50) not null,
-cpf varchar(15) not null,
-dataNascimento varchar not null,
-salario varchar(100) not null,
-cargo varchar not null
-)
-
-PASSO 3: 
-INSERT INTO funcionarios  (nome, sobrenome, cpf, datanascimento, salario, cargo) 
-VALUES ('Weslley Jonathan', 'Cezario Santana da Silva', '12345678900', '08/12/2000', '1.000', 'Engenheiro de software pleno');
+CREATE TABLE funcionarios (
+	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	nome varchar(30) NOT NULL,
+	sobrenome varchar(50) NOT NULL,
+	cpf varchar(15) NOT NULL,
+	datanascimento varchar NOT NULL,
+	salario varchar(100) NOT NULL,
+	cargo varchar NOT NULL,
+	CONSTRAINT funcionarios_pkey PRIMARY KEY (id)
+);
